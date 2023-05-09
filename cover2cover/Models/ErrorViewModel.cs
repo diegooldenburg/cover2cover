a1209fc9314092141a485acbe6f3e671a438e868
Diego Oldenburg <doldenburg@tulane.edu>
2023-05-09 15:04:01 -0500

    Add project files.

---
+++ cover2cover/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cover2cover.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
