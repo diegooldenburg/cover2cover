0eacdc94f7444a6ece4f56c8b55121af92c7cc3e
Diego Oldenburg <doldenburg@tulane.edu>
2023-05-10 18:12:44 -0500

    Identity scaffolding

---
+++ cover2cover/Areas/Identity/Data/cover2coverContext.cs
@@ -0,0 +1,34 @@
+﻿using cover2cover.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace cover2cover.Data;
+
+public class cover2coverContext : IdentityDbContext<cover2coverUser>
+{
+    public cover2coverContext(DbContextOptions<cover2coverContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new cover2coverUserEntityConfiguration());
+    }
+}
+
+public class cover2coverUserEntityConfiguration : IEntityTypeConfiguration<cover2coverUser>
+{
+    public void Configure(EntityTypeBuilder<cover2coverUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+    }
+}
